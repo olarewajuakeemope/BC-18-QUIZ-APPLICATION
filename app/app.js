8701459b3e3bed7787e1f8dd37d2f8dc6c1ca13c
adedapopaul <adedapopaul@yahoo.com>
2017-01-17 18:09:10 +0100

    Update app route

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
+// Test for express routing
+
+(function(){
+
 var express = require('express');
 var app = express();
 var path = require('path');
 
 var port = process.env.PORT || 3000;
-
 
 
 app.use(express.static(__dirname + '/view'));
@@ -14,15 +17,18 @@ app.set('view engine', 'html');
 
 app.get('/', function(req, res){
 	res.render('index.html')
+
 })
 
 app.get('/login', function(req, res){
 	res.render('login/login.html')
 })
 
+})();
 
 
 
-app.listen(port, function(){
-	console.log('listening on port 3000')
-});
+
+
+
+
