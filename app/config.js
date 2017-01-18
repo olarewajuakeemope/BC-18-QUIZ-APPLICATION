9085d31da1b51d4aaf72a1246f20d1562b7dd30a
adedapopaul <adedapopaul@yahoo.com>
2017-01-18 08:57:50 +0100

    Add firebase configuration file

---
+++ app/config.js
@@ -0,0 +1,19 @@
+
+var config = {
+    apiKey: "AIzaSyA3-v85rBZXODtmUzkHcwYVRYg6Khw_IAg",
+    authDomain: "quizapp-2105e.firebaseapp.com",
+    databaseURL: "https://quizapp-2105e.firebaseio.com",
+    storageBucket: "quizapp-2105e.appspot.com",
+    messagingSenderId: "80293514890"
+  };
+
+
+// var admin = require("firebase-admin");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert("path/to/serviceAccountKey.json"),
+//   databaseURL: "https://quizapp-2105e.firebaseio.com"
+// });
+
+
+
